1db3b982d74c993d2cf9c592628736b55663ea1f
Shanu Raj <shanuraj715@gmail.com>
2022-02-16 14:56:02 +0530

    Player Card Created

--- src/components/MyComps/FooterPlayer/FooterPlayer.js
+++ src/components/MyComps/FooterPlayer/FooterPlayer.js
@@ -29,7 +29,7 @@ export default class FooterPlayer extends Component {
 		muted: false,
 		loginVisible: false,
 		isFavorite: false,
-
+		playingSongData: {},
 		isExpanded: false
 	}
 
--- src/components/MyComps/Player/Player.js
+++ src/components/MyComps/Player/Player.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './player.css'
 import { Icon } from '../../Index'
 import conf from '../../../conf'
+import playerFunc from '../../../assets/js/player'
 import Queue from './comps/Queue/Queue'
 
 export default class Player extends Component {
@@ -12,6 +13,10 @@ export default class Player extends Component {
         document.body.style.minWidth = '100vw';
         document.body.style.maxWidth = '100vw';
         document.body.style.overflow = 'hidden';
+
+        this.interval = setInterval(() => {
+            this.scrubberOnChangeHandler()
+        }, 250)
     }
 
     componentWillUnmount() {
@@ -20,33 +25,62 @@ export default class Player extends Component {
         document.body.style.minWidth = 'initial';
         document.body.style.maxWidth = 'initial';
         document.body.style.overflow = 'initial';
+
+        clearInterval(this.interval)
     }
 
+    scrubberOnChangeHandler = () => {
+        const e = document.getElementById("scrubber")
+        const max = e.max
+        const value = e.value
+
+        e.style.backgroundSize = (value / max) * 100 + "%"
+
+
+    }
 
     render() {
-        console.log( this.props )
         return (
             <div className="full-player theme-light">
                 <Queue />
-
-                <div className="fp1-page-cont">
-                    <div className="fp1-player-cont-top">
-                        <div className="fp1-player-cont">
-                            <p className="fp1-app-name">{conf.APP_NAME}</p>
-                            <div className="fp1-song-img-cont">
-                                <img src="/favicon.png" alt="" className="fp1-song-img" />
+                <div className="player-background" style={{ backgroundImage: `url(${this.props.playingSongData.thumb})` }}></div>
+                <div className="player-page-container">
+                    <div className="player-top-site-name">
+                        <h2 className=""><span>{conf.APP_NAME}</span></h2>
+                    </div>
+                    <div className="player-card">
+                        <div className="player-card-left">
+                            <div className="player-thumb-cont">
+                                <img src={this.props.playingSongData.thumb} className="player-thumb" alt="" />
                             </div>
-                            <p className="fp1-song-title">Song Name Here</p>
-                            <div className="">
-                                <input type="range" id="scrubber" className="fp1-duration-slider" min="0" max={ this.props.maxScrubberVal}  onChange={e => this.props.seek(e.target.value)} />
+                        </div>
+                        <div className="player-card-right">
+                            <div className="player-data-row">
+                                <h2 className="pd-title">{this.props.playingSongData.title}</h2>
+                                <span className="pd-category">{this.props.playingSongData.category_name}</span>
                             </div>
-                            <div className="fp1-btn-cont">
-
+                            <div className="player-dta-row">
+                                <p className="pd-singer-cont">
+                                    <span className="pd-singer">{this.props.playingSongData.singer_name}</span>
+                                </p>
+                                <input type="range" id="scrubber" className="pd-scrubber" min="0" max={this.props.maxScrubberVal} onChange={e => this.props.seek(e.target.value)} />
+                                <div className="pd-time-cont">
+                                    <span className="pd-curr-time">{playerFunc.secToMinSec(this.props.currentTime).min}:{playerFunc.secToMinSec(this.props.currentTime).sec}</span>
+                                    <span className="pd-duration">{playerFunc.secToMinSec(this.props.maxScrubberVal).min}:{playerFunc.secToMinSec(this.props.maxScrubberVal).sec}</span>
+                                </div>
+                            </div>
+                            <div className="player-data-row player-control-btn-cont">
+                                <button className="pd-control-btn pd-prev-btn" onClick={() => { }}><Icon classes="fa-angle-double-left" type="solid" /></button>
+                                <button className="pd-control-btn pd-play-pause-btn" onClick={this.props.toggle}><Icon classes={`${this.props.playing ? 'fa-pause' : 'fa-play'}`} type="regular" /></button>
+                                <button className="pd-control-btn pd-next-btn" onClick={() => { }}><Icon classes="fa-angle-double-right" type="solid" /></button>
                             </div>
                         </div>
                     </div>
-                    <div className="fp1-player-fav-cont">
 
+                    <div className="">
+                        <p>
+                            Cards Here
+                        </p>
                     </div>
                 </div>
             </div>
